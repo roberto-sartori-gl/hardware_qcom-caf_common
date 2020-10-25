9d1cac82dc456fda62ec289aca20b02dfe13fdf1
Roberto Sartori <roberto.sartori.android@gmail.com>
2020-10-25 22:37:40 +0100

    os_pickup_aosp: include the AOSP wlan directory
    This is an hack: we need the wifi HAL to be in the same directory as the
    AOSP HAL, so include this directory when BOARD_USES_QCOM_HARDWARE is
    true.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- os_pickup_aosp.mk
+++ os_pickup_aosp.mk
@@ -6,6 +6,7 @@ LOCAL_PATH := $(call my-dir)
 # Legacy CAF devices still want AOSP keymaster HAL.
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include $(LOCAL_PATH)/keymaster/Android.mk
+include hardware/qcom/wlan/Android.mk
 else
 include $(call first-makefiles-under,$(LOCAL_PATH))
 endif
